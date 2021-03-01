3ea43055a41e6356f52e958cb2f04b66747ff35a
yanzhen <sdyzsdut@163.com>
2021-03-01 15:37:33 +0800

    Initial Commit

---
+++ YZBGRAShow/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  YZBGRAShow
+//
+//  Created by yanzhen on 2021/3/1.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
