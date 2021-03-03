75b1970c15c14ebc91f9cdde2f000751a9c990e4
yanzhen <sdyzsdut@163.com>
2021-03-03 17:17:12 +0800

    test

--- YZBGRAShow/LibYUV/BGRA/LBGRAViewController.m
+++ YZBGRAShow/LibYUV/BGRA/LBGRAViewController.m
@@ -9,10 +9,13 @@
 #import "VideoBGRACapture.h"
 #import <YZLibyuv/YZLibyuv.h>
 
-@interface LBGRAViewController ()
+@interface LBGRAViewController ()<VideoBGRACaptureDelegate>
 @property (weak, nonatomic) IBOutlet UIImageView *mainPlayer;
 @property (weak, nonatomic) IBOutlet UIImageView *showPlayer;
 
+@property (nonatomic, strong) VideoBGRACapture *capture;
+
+@property (nonatomic, strong) CIContext *context;
 @end
 
 @implementation LBGRAViewController
@@ -21,8 +24,56 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [YZLibyuv test];
+    
+    _context = [CIContext contextWithOptions:nil];
+    
+    _capture = [[VideoBGRACapture alloc] initWithPlayer:_mainPlayer];
+    _capture.delegate = self;
+    [_capture startRunning];
 }
 
+#pragma mark - VideoBGRACaptureDelegate
+- (void)capture:(VideoBGRACapture *)capture pixelBuffer:(CVPixelBufferRef)pixelBuffer {
+    
+}
+
+#pragma mark - helper
+- (void)showPixelBuffer:(CVPixelBufferRef)pixel {
+    CVPixelBufferRetain(pixel);
+    CIImage *ciImage = [CIImage imageWithCVImageBuffer:pixel];
+    size_t width = CVPixelBufferGetWidth(pixel);
+    size_t height = CVPixelBufferGetHeight(pixel);
+    CGImageRef videoImageRef = [_context createCGImage:ciImage fromRect:CGRectMake(0, 0, width, height)];
+    UIImage *image = [UIImage imageWithCGImage:videoImageRef];
+    CGImageRelease(videoImageRef);
+    CVPixelBufferRelease(pixel);
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self.showPlayer.image = image;
+    });
+}
+
+- (int)getOutputRotation {//test code
+    int ratation = 0;
+    UIInterfaceOrientation orientation = UIApplication.sharedApplication.statusBarOrientation;
+    switch (orientation) {
+        case UIInterfaceOrientationPortrait:
+            return 90;
+            break;
+        case UIInterfaceOrientationPortraitUpsideDown:
+            return 270;
+            break;
+        case UIInterfaceOrientationLandscapeLeft:
+            return 0;
+            break;
+        case UIInterfaceOrientationLandscapeRight:
+            return 180;
+            break;
+        default:
+            break;
+    }
+    return ratation;
+    
+}
 #pragma mark - UI
 - (IBAction)exitCapture:(id)sender {
     [self.navigationController popViewControllerAnimated:YES];
