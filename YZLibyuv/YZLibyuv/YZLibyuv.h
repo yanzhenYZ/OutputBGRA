95d74c3451d3018b2b164e6d0935d4088b395f65
yanzhen <sdyzsdut@163.com>
2021-03-03 17:32:21 +0800

    add libyuv files

---
+++ YZLibyuv/YZLibyuv/YZLibyuv.h
@@ -0,0 +1,14 @@
+//
+//  YZLibyuv.h
+//  YZLibyuv
+//
+//  Created by yanzhen on 2021/3/3.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YZLibyuv : NSObject
++ (void)test;
+@end
+
+
---
+++ YZLibyuv/YZLibyuv/YZLibyuv.m
@@ -0,0 +1,14 @@
+//
+//  YZLibyuv.m
+//  YZLibyuv
+//
+//  Created by yanzhen on 2021/3/3.
+//
+
+#import "YZLibyuv.h"
+
+@implementation YZLibyuv
++(void)test {
+    NSLog(@"1234:test");
+}
+@end
