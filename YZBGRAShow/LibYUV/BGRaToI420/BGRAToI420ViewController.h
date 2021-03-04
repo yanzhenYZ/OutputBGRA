92d8cc875fa04b29e08c159857fc4d057dff9c04
yanzhen <sdyzsdut@163.com>
2021-03-04 21:06:11 +0800

    todo BGRAToI420

---
+++ YZBGRAShow/LibYUV/BGRaToI420/BGRAToI420ViewController.h
@@ -0,0 +1,16 @@
+//
+//  BGRAToI420ViewController.h
+//  YZBGRAShow
+//
+//  Created by yanzhen on 2021/3/4.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BGRAToI420ViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YZBGRAShow/LibYUV/BGRaToI420/BGRAToI420ViewController.m
@@ -0,0 +1,90 @@
+//
+//  BGRAToI420ViewController.m
+//  YZBGRAShow
+//
+//  Created by yanzhen on 2021/3/4.
+//
+
+#import "BGRAToI420ViewController.h"
+#import "VideoBGRACapture.h"
+#import <YZLibyuv/YZLibyuv.h>
+
+@interface BGRAToI420ViewController ()<VideoBGRACaptureDelegate, YZLibyuvDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *mainPlayer;
+@property (weak, nonatomic) IBOutlet UIImageView *showPlayer;
+
+@property (nonatomic, strong) VideoBGRACapture *capture;
+@property (nonatomic, strong) CIContext *context;
+@end
+
+@implementation BGRAToI420ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    _context = [CIContext contextWithOptions:nil];
+    
+    _capture = [[VideoBGRACapture alloc] initWithPlayer:_mainPlayer];
+    _capture.delegate = self;
+    [_capture startRunning];
+}
+
+#pragma mark - VideoBGRACaptureDelegate
+- (void)capture:(VideoBGRACapture *)capture pixelBuffer:(CVPixelBufferRef)pixelBuffer {
+    
+}
+
+
+#pragma mark - helper
+- (void)showPixelBuffer:(CVPixelBufferRef)pixel {
+    CVPixelBufferRetain(pixel);
+    CIImage *ciImage = [CIImage imageWithCVImageBuffer:pixel];
+    size_t width = CVPixelBufferGetWidth(pixel);
+    size_t height = CVPixelBufferGetHeight(pixel);
+    CGImageRef videoImageRef = [_context createCGImage:ciImage fromRect:CGRectMake(0, 0, width, height)];
+    UIImage *image = [UIImage imageWithCGImage:videoImageRef];
+    CGImageRelease(videoImageRef);
+    CVPixelBufferRelease(pixel);
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self.showPlayer.image = image;
+    });
+}
+
+- (int)getOutputRotation {//test code
+    int ratation = 0;
+    UIInterfaceOrientation orientation = UIApplication.sharedApplication.statusBarOrientation;
+    switch (orientation) {
+        case UIInterfaceOrientationPortrait:
+            return 90;
+            break;
+        case UIInterfaceOrientationPortraitUpsideDown:
+            return 270;
+            break;
+        case UIInterfaceOrientationLandscapeLeft:
+            return 0;
+            break;
+        case UIInterfaceOrientationLandscapeRight:
+            return 180;
+            break;
+        default:
+            break;
+    }
+    return ratation;
+    
+}
+- (IBAction)exitCapture:(id)sender {
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    self.navigationController.navigationBarHidden = YES;
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    self.navigationController.navigationBarHidden = NO;
+}
+
+@end
