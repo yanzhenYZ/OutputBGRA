2b0fe3ec64b2b40d15d4517d2b0d4a52243d9f7a
yanzhen <sdyzsdut@163.com>
2021-03-04 15:21:34 +0800

    video range

---
+++ YZLibyuv/YZLibyuv/Tool/PixelBuffer/LibyuvVideoRangePixelBuffer.h
@@ -0,0 +1,16 @@
+//
+//  LibyuvVideoRangePixelBuffer.h
+//  YZLibyuv
+//
+//  Created by yanzhen on 2021/3/4.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LibyuvVideoRangePixelBuffer : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YZLibyuv/YZLibyuv/Tool/PixelBuffer/LibyuvVideoRangePixelBuffer.m
@@ -0,0 +1,12 @@
+//
+//  LibyuvVideoRangePixelBuffer.m
+//  YZLibyuv
+//
+//  Created by yanzhen on 2021/3/4.
+//
+
+#import "LibyuvVideoRangePixelBuffer.h"
+
+@implementation LibyuvVideoRangePixelBuffer
+
+@end
