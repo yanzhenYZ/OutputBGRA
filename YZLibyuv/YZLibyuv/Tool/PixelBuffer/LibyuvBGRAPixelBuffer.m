11f9bf1f9ab7085341f11176ef33d53c474bada7
yanzhen <sdyzsdut@163.com>
2021-03-04 11:30:42 +0800

    bgra

--- YZLibyuv/YZLibyuv/Tool/PixelBuffer/LibyuvBGRAPixelBuffer.m
+++ YZLibyuv/YZLibyuv/Tool/PixelBuffer/LibyuvBGRAPixelBuffer.m
@@ -24,26 +24,28 @@
         [_buffer inputVideoData:videoData pixelBuffer:videoData.pixelBuffer];
     } else {//todo
         CVPixelBufferRef pixelBuffer = videoData.pixelBuffer;
-        size_t width = CVPixelBufferGetWidth(pixelBuffer);
-        size_t height = CVPixelBufferGetHeight(pixelBuffer);
+        size_t w = CVPixelBufferGetWidth(pixelBuffer);
+        size_t h = CVPixelBufferGetHeight(pixelBuffer);
+        int width = (int)w;
+        int height = (int)h;
+        if (videoData.rotation == 90 || videoData.rotation ==  270) {
+            width = (int)h;
+            height = (int)w;
+        }
+        
         uint8_t *buffer = malloc(width * height * 4);
         CVPixelBufferLockBaseAddress(pixelBuffer, 0);
         uint8_t *srcBuffer = CVPixelBufferGetBaseAddress(pixelBuffer);
-        size_t bytesPerrow = CVPixelBufferGetBytesPerRow(pixelBuffer);
-        size_t dstStride = width * 4;
-        int pw = width;
-        int ph = height;
-        if (videoData.rotation == 90 || videoData.rotation == 270) {
-            pw = height;
-            ph = width;
-            dstStride = height * 4;
-        }
-        [YZLibyuvTool ARGBRotate:srcBuffer srcStride:bytesPerrow / 4 dstBuffer:buffer dstStride:dstStride width:width height:height rotation:videoData.rotation];
+        int bytesPerrow = (int)CVPixelBufferGetBytesPerRow(pixelBuffer);
+        int dstStride = width * 4;
+        
+        
+        [YZLibyuvTool ARGBRotate:srcBuffer srcStride:bytesPerrow dstBuffer:buffer dstStride:dstStride width:width height:height rotation:videoData.rotation];
         CVPixelBufferUnlockBaseAddress(pixelBuffer, 0);
         
         
         CVPixelBufferRef newPixelBuffer = NULL;
-        CVPixelBufferCreateWithBytes(kCFAllocatorDefault, pw, ph, kCVPixelFormatType_32BGRA, buffer, pw * 4,
+        CVPixelBufferCreateWithBytes(kCFAllocatorDefault, width, height, kCVPixelFormatType_32BGRA, buffer, width * 4,
                                      NULL, NULL, NULL, &newPixelBuffer);
         if (pixelBuffer != NULL) {
             [_buffer inputVideoData:videoData pixelBuffer:newPixelBuffer];
--- YZLibyuv/YZLibyuv/Tool/YZLibyuvTool.mm
+++ YZLibyuv/YZLibyuv/Tool/YZLibyuvTool.mm
@@ -11,8 +11,6 @@
 @implementation YZLibyuvTool
 + (void)ARGBRotate:(uint8 *)srcBuffer srcStride:(int)srcStride dstBuffer:(uint8_t *)dstBuffer dstStride:(int)dstStride width:(int)width height:(int)height rotation:(int)rotation {
     libyuv::RotationMode mode = libyuv::kRotate0;
-    int dstW = width;
-    int dstHeight = height;
     if (rotation == 90) {
         mode = libyuv::kRotate90;
     } else if (rotation == 180) {
@@ -20,7 +18,7 @@
     } else if (rotation == 270) {
         mode = libyuv::kRotate0;
     }
-    libyuv::ARGBRotate(srcBuffer, srcStride, dstBuffer, dstStride, dstW, dstHeight, mode);
+    libyuv::ARGBRotate(srcBuffer, srcStride, dstBuffer, dstStride, width, height, mode);
 }
 
 @end
